c015c30b3b1d9b37361298cf3c4c85a3e9c52c90
Tiwari <sanjeet.tiwari@siemens.com>
2019-10-02 23:47:31 +0530

    feat: modified text field for sending texts

--- src/components/FirstPage/FirstPage.jsx
+++ src/components/FirstPage/FirstPage.jsx
@@ -3,13 +3,10 @@ import './FirstPage.css';
 import {
     Button
 } from 'react-bootstrap';
-import * as QRCode from 'qrcode.react';
 import { setTicket } from '../../state/actions/processActions';
 import { generateTicket } from '../../utils/ticketGenerator';
-import { getProcessData } from '../../state/providers/processProvider';
 
 export const FirstHelloPage = (props) => {
-    const [qrcode, toggleQrCode] = useState(false);
     return (
         <div className="firstpage">
             Hi There!
@@ -17,38 +14,24 @@ export const FirstHelloPage = (props) => {
             Welcome to <span className="protitle">Wormhole</span>!
             <br />
             <p className="description">Using Wormhole, you can connect and share with multiple devices!</p>
-            {(!qrcode)
-            ?
             <>
-                <Button variant="outline-info" onClick={generateATicket}>Connect</Button>
+                <Button variant="outline-info" onClick={connectToOther}>Connect</Button>
                 &nbsp; / &nbsp;
                 <Button variant="outline-success" onClick={goToTalkingPage}>Create</Button>
             </>
-            :
-            <>
-                <br />
-                <br />
-                <QRCode value={getProcessData('ticket')} />
-                <br />
-                <p className="description">Scan this QR Code from your Wormhole App</p>
-                <br />
-                <Button onClick={cancelQrCode} variant="outline-secondary">Go Back</Button>
-            </>
-            }
         </div>
     );
 
-    function generateATicket() {
-        setTicket(generateTicket());
-        toggleQrCode(true);
-    }
-
-    function cancelQrCode() {
-        setTicket('');
-        toggleQrCode(false);
+    function connectToOther() {
+        // here the code will be written for scanning the Qr Code
     }
 
     function goToTalkingPage() {
+        generateATicket();
         props.history.push('/talk');
+    }
+
+    function generateATicket() {
+        setTicket(generateTicket());
     }
 }
--- src/components/TalkingPage/TalkingPage.jsx
+++ src/components/TalkingPage/TalkingPage.jsx
@@ -1,19 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TalkingPage.css';
-import { FormControl } from 'react-bootstrap';
+import {
+    FormControl,
+    Modal,
+    InputGroup,
+    Button
+} from 'react-bootstrap';
+import { getProcessData } from '../../state/providers/processProvider';
+import * as QRCode from 'qrcode.react';
 
 export const TalkingPage = () => {
+    const [qrCode, showHideQr] = useState(false);
+    const [text, setText] = useState('');
     return (
         <div className="talkingpage">
             <div className="actionbar">
                 <div className="actionbtn">
                     <i className="fas fa-plus"></i>
                 </div>
-                <FormControl placeholder="Enter Text Here" className="textarea"/>
-                <div className="actionbtn">
+                <InputGroup className="textarea">
+                    <FormControl
+                        placeholder="Enter Text Here"
+                        onChange={(event) => setText(event.target.value)}/>
+                    <InputGroup.Append>
+                        <Button variant="outline-secondary" onClick={sendText}>Send</Button>
+                    </InputGroup.Append>
+                </InputGroup>
+                <div className="actionbtn" onClick={showQrCode}>
                     <i className="fas fa-qrcode"></i>
                 </div>
             </div>
+            <QrCode show={qrCode} onHide={hideQrCode}/>
         </div>
     );
+
+    function showQrCode() {
+        showHideQr(true);
+    }
+
+    function hideQrCode() {
+        showHideQr(false);
+    }
+
+    function sendText() {
+        // make an API call to send the text.
+        console.log(text);
+    }
+}
+
+export const QrCode = (props) => {
+    return (
+        <Modal show={props.show} onHide={props.onHide} centered>
+            <Modal.Body style={{ textAlign: 'center' }}>
+                <QRCode value={getProcessData('ticket')} />
+                <br />
+                <br />
+                Scan this QR Code to Connect
+            </Modal.Body>
+        </Modal>
+    )
 }
